41bb3fdd0a4fa26d7e131d5c850d1d31ac7a6e94
xiDDdw <xiDDdw@gmail.com>
2011-12-12 19:13:38 +0000

    Loading scaled images.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,12 +3,61 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    imageDialog = new QFileDialog(this);
+    imageDialog->setFileMode(QFileDialog::ExistingFile);
+    imageDialog->setNameFilter(tr("Images (*.png *.jpg *bmp)"));
+    imageDialog->setViewMode(QFileDialog::Detail);
+
+    img1 = new QImage();
+    img2 = new QImage();
+
+    scen1 = new QGraphicsScene();
+    scen2 = new QGraphicsScene();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+    delete imageDialog;
+}
+
+void MainWindow::on_pushButton1_clicked() {
+    QStringList fileNames;
+    if (!imageDialog->exec()) return;
+
+    fileNames = imageDialog->selectedFiles();
+
+    QString imagePath = fileNames.first();
+
+    QSize scont = ui->graphicsView1->size();
+    scont -= QSize(5, 5);
+
+    img1->load(imagePath);
+
+    scen1->clear();
+    scen1->addPixmap(QPixmap::fromImage(img1->scaled(scont, Qt::KeepAspectRatio)));
+
+    ui->graphicsView1->setScene(scen1);
+}
+
+void MainWindow::on_pushButton2_clicked() {
+    QStringList fileNames;
+    if (!imageDialog->exec()) return;
+
+    fileNames = imageDialog->selectedFiles();
+
+    QString imagePath = fileNames.first();
+
+    QSize scont = ui->graphicsView1->size();
+    scont -= QSize(5, 5);
+
+    img2->load(imagePath);
+
+    scen2->clear();
+    scen2->addPixmap(QPixmap::fromImage(img2->scaled(scont, Qt::KeepAspectRatio)));
+
+    ui->graphicsView2->clearMask();
+    ui->graphicsView2->setScene(scen2);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
 
 namespace Ui {
     class MainWindow;
@@ -15,8 +19,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_pushButton1_clicked();
+
+    void on_pushButton2_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    QFileDialog *imageDialog;
+
+    QImage *img1, *img2;
+
+    QGraphicsScene *scen1, *scen2;
 };
 
 #endif // MAINWINDOW_H
