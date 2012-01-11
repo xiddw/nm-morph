9d1d01bb7fa97412ea180fc9e02bc216e9617d3a
xiDDdw@gmail.com <xiDDdw@gmail.com@a017fcb9-5a62-b076-ef28-7a214fec36fe>
2012-01-11 01:17:35 +0000

    Optimized some code :)

--- trunk/window.cpp
+++ trunk/window.cpp
@@ -1,89 +1,92 @@
 #include "window.h"
 
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
-    colorPen = new QColor(Qt::red);
+    //colorPen = new QColor(Qt::red);
     wimg = himg = 0;
     VARA = VARP = 0.0f;
 
     this->setMinimumSize(600, 440);
     this->resize(1000, 660);
-    this->setStyleSheet("QAbstractButton, QLabel { padding: 10px; };");
+    this->setStyleSheet("QAbstractButton, QLabel { padding: 6px 10px; };");
 
+    // Layout para toda la ventana
     QHBoxLayout *superLayout = new QHBoxLayout();
 
+    // Layout para componentes principales
+    // (cargar imagenes y controles)
     QVBoxLayout *mainLayout = new QVBoxLayout();
-    QSplitter *mainSplit = new QSplitter(Qt::Horizontal);
 
+    // Layout para cargar imagenes
+    QHBoxLayout *imgLayout = new QHBoxLayout();
+
+    // Layout para controles
     QHBoxLayout *headLayout = new QHBoxLayout();
 
+    // Layout para Tipo de linea a usar
     QGroupBox *opcGrp1 = new QGroupBox(tr("Tipo de linea que desea usar"));
     opcGrp1->setMinimumHeight(80);
     opcGrp1->setMaximumHeight(80);
-    //opcGrp1->setMinimumWidth(300);
-
     QFormLayout *opcForm1 = new QFormLayout();
     opcGrp1->setLayout(opcForm1);
-
     radio[0] = new QRadioButton(tr("Segmentos de lineas rectas"));
-    radio[0]->setStyleSheet("padding: 1px");
-    connect(radio[0], SIGNAL(toggled(bool)), this, SLOT(on_radioLinea_toogled(bool)));
-
-    radio[0]->setChecked(true);
-    opcForm1->addRow(radio[0]);
-
+    radio[0]->setStyleSheet("padding: 1px");    
+    radio[0]->setChecked(true);    
     radio[1] = new QRadioButton(tr("Linea continua a mano"));
     radio[1]->setStyleSheet("padding: 1px");
+    connect(radio[0], SIGNAL(toggled(bool)), this, SLOT(on_radioLinea_toogled(bool)));
     connect(radio[1], SIGNAL(toggled(bool)), this, SLOT(on_radioLinea_toogled(bool)));
+    opcForm1->addRow(radio[0]);
     opcForm1->addRow(radio[1]);
 
-    headLayout->addWidget(opcGrp1);
-    mainLayout->addLayout(headLayout);
-
+    // Layout para Controles basicos
     QGroupBox *opcGrp2 = new QGroupBox(tr("Controles"));
-    //opcGrp2->setMinimumWidth(300);
     opcGrp2->setMinimumHeight(150);
     opcGrp2->setMaximumHeight(150);
-
     QFormLayout *opcForm2 = new QFormLayout();
     opcGrp2->setLayout(opcForm2);
-
     btnColor = new QPushButton("Color de pincel: ");
-    lblColor = new QLabel(colorPen->name());
-    lblColor->setPalette(QPalette(colorPen->rgb()));
-    lblColor->setAutoFillBackground(true);
-
+    btnProcess = new QPushButton("Realizar morphing");
+    btnSave = new QPushButton("Guardar imagen resultante");
+    lblColor = new QLabel();
+    lblColor->setAutoFillBackground(true);        
+    connect(btnColor,   SIGNAL(clicked()), this, SLOT(on_btnColor_clicked()));
+    connect(btnProcess, SIGNAL(clicked()), this, SLOT(on_btnProcess_clicked()));
+    connect(btnSave,    SIGNAL(clicked()), this, SLOT(on_btnSave_clicked()));
     opcForm2->addRow(lblColor, btnColor);
-    connect(btnColor, SIGNAL(clicked()), this, SLOT(on_btnColor_clicked()));
+    opcForm2->addRow(btnProcess);
+    opcForm2->addRow(btnSave);
 
+    // Layout para Parametros de morphing
     QGroupBox *opcGrp3 = new QGroupBox(tr("Parámetros"));
     opcGrp3->setStyleSheet("QLabel { padding: 1px; }");
     opcGrp3->setMaximumWidth(200);
     opcGrp3->setMaximumHeight(150);
-
     QFormLayout *opcForm3 = new QFormLayout();
     opcGrp3->setLayout(opcForm3);
-
     txtvalA = new QLineEdit();
     txtvalA->setAlignment(Qt::AlignRight);
     txtvalA->setText("0.001");
-
+    connect(txtvalA, SIGNAL(returnPressed()), this, SLOT(on_txtValEnter()));
     txtvalB = new QLineEdit();
     txtvalB->setAlignment(Qt::AlignRight);
     txtvalB->setText("2");
-
+    connect(txtvalB, SIGNAL(returnPressed()), this, SLOT(on_txtValEnter()));
     txtvalP = new QLineEdit();
     txtvalP->setAlignment(Qt::AlignRight);
     txtvalP->setText("0");
-
+    connect(txtvalP, SIGNAL(returnPressed()), this, SLOT(on_txtValEnter()));
     opcForm3->addRow(tr("A:"), txtvalA);
     opcForm3->addRow(tr("B:"), txtvalB);
     opcForm3->addRow(tr("P:"), txtvalP);
 
+    // Agregar layouts de controles a cabecera
+    headLayout->addWidget(opcGrp1);
     headLayout->addWidget(opcGrp3);
     headLayout->addWidget(opcGrp2);
+    mainLayout->addLayout(headLayout);
 
-    mainSplit->setChildrenCollapsible(false);
 
+    // Inicializar controles para imagenes
     imgs[4] = new QImage();
     view[4] = new GraphicsView();
     view[4]->enableDrawing(false);
@@ -103,12 +106,11 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
         scen[i+2] = new QGraphicsScene();
 
         btnUndo[i] = new QPushButton("Deshacer ultimo trazo");
-        connect(btnUndo[i], SIGNAL(clicked()), this, SLOT(on_btnUndo_clicked()));
-
         btnClear[i] = new QPushButton("Limpiar lienzo");
-        connect(btnClear[i],SIGNAL(clicked()),this,SLOT(on_btnClear_clicked()));
-
         btnOpen[i] = new QPushButton(tr("Cargar imagen"));
+
+        connect(btnUndo[i], SIGNAL(clicked()), this, SLOT(on_btnUndo_clicked()));
+        connect(btnClear[i],SIGNAL(clicked()),this,SLOT(on_btnClear_clicked()));
         connect(btnOpen[i], SIGNAL(clicked()), this, SLOT(on_btnOpen_clicked()));
 
         imageContainer[i] = new QVBoxLayout();
@@ -121,56 +123,40 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
         frame[i] = new QFrame();
         frame[i]->setLayout(imageContainer[i]);
 
-        mainSplit->addWidget(frame[i]);
+        imgLayout->addWidget(frame[i]);
 
         view[i]->setScene(scen[i]);
         view[i+2]->setScene(scen[i+2]);
     }
 
+    // Para las imagenes a la derecha
     imageContainer[2] = new QVBoxLayout();
-
-    btnProcess = new QPushButton("Realizar morphing");
-    connect(btnProcess, SIGNAL(clicked()), this, SLOT(on_btnProcess_clicked()));
-    opcForm2->addRow(btnProcess);
-
-    btnSave = new QPushButton("Guardar imagen resultante");
-    connect(btnSave, SIGNAL(clicked()), this, SLOT(on_btnSave_clicked()));
-    opcForm2->addRow(btnSave);
-
     imageContainer[2]->addWidget(view[2]);
     imageContainer[2]->addWidget(view[3]);
     imageContainer[2]->addWidget(view[4]);
-
     frame[2] = new QFrame();
     frame[2]->setLayout(imageContainer[2]);
     frame[2]->setMaximumWidth(300);
 
-    mainLayout->addWidget(mainSplit);
-
+    // Layout de ventana
+    mainLayout->addLayout(imgLayout);
     superLayout->addLayout(mainLayout);
     superLayout->addWidget(frame[2]);
-
     this->setLayout(superLayout);
 
+    // Configuracion del dialogo para cargar imágenes
     diaImage = new QFileDialog(this);
     diaImage->setFileMode(QFileDialog::ExistingFile);
     diaImage->setNameFilter(tr("Images (*.png *.jpg *bmp)"));
     diaImage->setViewMode(QFileDialog::Detail);
 
     diaColor = new QColorDialog(this);
-    diaColor->setCurrentColor(colorPen->rgb());
+    //diaColor->setCurrentColor(colorPen->rgb());
+    this->ChangeColorPen(QColor(Qt::red));
 }
 
 MainWindow::~MainWindow() {
-//    delete imageDialog;
 
-//    delete[] imgs;
-//    delete[] scen;
-//    delete[] view;
-//    delete[] btnOpen;
-
-//    delete[] frame;
-//    delete[] imageContainer;
 }
 
 void MainWindow::keyPressEvent(QKeyEvent *e) {
@@ -181,6 +167,10 @@ void MainWindow::keyPressEvent(QKeyEvent *e) {
     }
 }    
 
+void MainWindow::on_txtValEnter() {
+    this->ArreglameLaVida();
+}
+
 void MainWindow::on_radioLinea_toogled(bool a) {
     GraphicsView::straightLine = (sender() == radio[!a] && a);
 }
@@ -190,39 +180,51 @@ void MainWindow::on_btnOpen_clicked() {
     LoadImage(pos);
 }
 
-
+// Limpia el widget contenedor de la imagen
 void MainWindow::on_btnClear_clicked() {
     bool pos = (sender() == btnClear[RIGTH]);
     CleanCanvas(pos);
 }
 
+// Deshace último trazo realizado
 void MainWindow::on_btnUndo_clicked() {
     bool pos = (sender() == btnUndo[RIGTH]);
     UndoLineCanvas(pos);
 }
 
+// Cambia el color del pincel con el que se dibuja
 void MainWindow::on_btnColor_clicked() {
     if(!diaColor->exec()) return;
-
-    *colorPen = diaColor->selectedColor();
-    lblColor->setText(colorPen->name());
-    lblColor->setPalette(QPalette(colorPen->rgb()));
-    GraphicsView::colorDrawing(*colorPen);
+    this->ChangeColorPen(diaColor->selectedColor());
 }
 
-double dotProd(QPoint a, QPoint b) {
-    return (a.x()*b.x() + a.y()*b.y());
+void MainWindow::ChangeColorPen(QColor colorPen) {
+    diaColor->setCurrentColor(colorPen);
+    lblColor->setText(colorPen.name());
+    lblColor->setPalette(QPalette(colorPen.rgb()));
+    GraphicsView::colorDrawing(colorPen);
 }
 
-double norma(QPoint a) {
-    return sqrt(pow(a.x(), 2.0) + pow(a.y(), 2.0));
-}
+//double dotProd(QPoint a, QPoint b) {
+//    return (a.x()*b.x() + a.y()*b.y());
+//}
 
-double norma2(QPoint a) {
-    return (pow(a.x(), 2.0) + pow(a.y(), 2.0));
-}
+//double norma(QPoint a) {
+//    return sqrt(pow(a.x(), 2.0) + pow(a.y(), 2.0));
+//}
 
+//double norma2(QPoint a) {
+//    return (pow(a.x(), 2.0) + pow(a.y(), 2.0));
+//}
+
+
+// Realizar el morphing entre las imagenes, de acuerdo a los trazos
 void MainWindow::on_btnProcess_clicked() {
+    this->ArreglameLaVida();
+}
+
+void MainWindow::ArreglameLaVida() {
+    // Obtener parametros para el morph
     VARA = this->txtvalA->text().toDouble();
     VARB = this->txtvalB->text().toDouble();
     VARP = this->txtvalP->text().toDouble();
@@ -267,8 +269,6 @@ void MainWindow::on_btnProcess_clicked() {
     QRgb b = qRgb(0, 0, 255);
 
     if(GraphicsView::straightLine) {
-
-
         int lenght = view[0]->listLine->size();
 
         for(int k=0; k<lenght; ++k) {
@@ -291,23 +291,22 @@ void MainWindow::on_btnProcess_clicked() {
                 y3 += y1 / 2;
                 y4 += y2 / 2;
 
+                //Si x esta mas disperso que y
                 if(fabs(x1-x2) > fabs(y1-y2)) {
-                    if(x1 > x2) {
-                        double x = x1; x1 = x2; x2 = x;
-                        double y = y1; y1 = y2; y2 =y ;
-                    }
+                    // Cambiar si coordenadas estan al reves
+                    if(x1 > x2) { swap(x1, x2); swap(y1, y2); }
 
+                    // Ir trazando linea de acuerdo a la pendiente obtenida
                     while(x1 < x2) {
                         y1 = m*(x1 - x2) + y2;
                         imgs[4]->setPixel(x1, y1, h?r:b);
                         x1 += 0.01;
                     }
                 } else {
-                    if(y1 > y2) {
-                        double x = x1; x1 = x2; x2 = x;
-                        double y = y1; y1 = y2; y2 =y ;
-                    }
+                    // Cambiar si coordenadas estan al reves
+                    if(y1 > y2) { swap(x1, x2); swap(y1, y2); }
 
+                    // Ir trazando linea de acuerdo a la pendiente obtenida
                     while(y1 < y2) {
                         x1 = (y1 - y2)/m + x2;
                         imgs[4]->setPixel(x1, y1, h?r:b);
@@ -324,24 +323,20 @@ void MainWindow::on_btnProcess_clicked() {
 
             //Si x esta mas disperso que y
             if(fabs(x4-x3) > fabs(y4-y3)) {
-                // Si coordenadas estan al reves
-                if(x3 > x4) {
-                    double x = x3; x3 = x4; x4 = x;
-                    double y = y3; y3 = y4; y4 = y;
-                }
+                // Cambiar si coordenadas estan al reves
+                if(x3 > x4) { swap(x3, x4); swap(y3, y4); }
 
+                // Ir trazando linea de acuerdo a la pendiente obtenida
                 while(x3 < x4) {
                     y3 = m*(x3 - x4) + y4;
                     imgs[4]->setPixel(x3, y3, qRgb(0, 0, 0));
                     x3 += 0.01;
                 }
             } else {
-                // Si coordenadas estan al reves
-                if(y3 > y4) {
-                    double x = x3; x3 = x4; x4 = x;
-                    double y = y3; y3 = y4; y4 = y;
-                }
+                // Cambiar si coordenadas estan al reves
+                if(y3 > y4) { swap(x3, x4); swap(y3, y4); }
 
+                // Ir trazando linea de acuerdo a la pendiente obtenida
                 while(y3 < y4) {
                     x3 = (y3 - y4)/m + x4;
                     imgs[4]->setPixel(x3, y3, qRgb(0, 0, 0));
@@ -373,42 +368,38 @@ void MainWindow::on_btnProcess_clicked() {
                         QPoint P = view[h]->listLine->at(k).first;
                         QPoint Q = view[h]->listLine->at(k).second;
 
-                        QPoint XP = X - P;
-                        QPoint QP = Q - P;
+                        QVector2D XP(X - P);
+                        QVector2D QP(Q - P);
 
-                        QPoint pQP;
-                        pQP.setX(QP.y());
-                        pQP.setY(-QP.x());
+                        QVector2D pQP(QP.y(), -QP.x());
 
                         // Calcular u, v
-                        u = dotProd(XP, QP) / norma2(QP);
-                        v = dotProd(XP, pQP) / norma(QP);
+                        u = QVector2D::dotProduct(XP, QP) /  QP.lengthSquared();
+                        v = QVector2D::dotProduct(XP, pQP) / QP.length();
 
                         // Obtener puntos interpolados de linea de referencia
                         QPoint P2 = view[h]->listAux->at(k).first;
                         QPoint Q2 = view[h]->listAux->at(k).second;
 
-                        QPoint Q2P2 = Q2 - P2;
-                        QPoint pQ2P2;
-                        pQ2P2.setX(Q2P2.y());
-                        pQ2P2.setY(-Q2P2.x());
+                        QVector2D Q2P2(Q2 - P2);
+                        QVector2D pQ2P2(Q2P2.y(), -Q2P2.x());
 
-                        QPoint X2 = P2 + u*Q2P2 + (v * pQ2P2) / norma(Q2P2);
-                        QPoint p = X2 - X;
+                        QVector2D X2 = QVector2D(P2) + u * Q2P2 + (v * pQ2P2) / Q2P2.length();
+
+                        QPoint p = X2.toPoint() - X;
 
                         double dist = 0;
                         if(u > 0 && u < 1) dist = fabs(v);
-                        else if(u < 0) dist = sqrt(pow(X.x() - P.x(), 2.0) + pow(X.y() - P.y(), 2.0));
+                        else if(u <= 0) dist = sqrt(pow(X.x() - P.x(), 2.0) + pow(X.y() - P.y(), 2.0));
                         else dist = sqrt(pow(X.x() - Q.x(), 2.0) + pow(X.y() - Q.y(), 2.0));
 
                         double w = 0;
-                        w =  pow(norma(QP), VARP);
+                        w =  pow(QP.length(), VARP);
                         w /= (VARA + dist);
                         w = pow(w, VARB);
 
                         ww[k] = w;
                         pp[k] = p;
-                        //posibles->push_back(make_pair(p, w));
                     }
 
                     QPoint sum(0.0, 0.0);
@@ -417,36 +408,28 @@ void MainWindow::on_btnProcess_clicked() {
                         sum  += ww[k] * pp[k];
                         wsum += ww[k];
                     }
-
                     sum /= wsum;
 
                     QPoint X2 = X + sum;
 
-
                     double y0, x0;
-                    if(X2.y() < himg && X2.y() > 0.5) {
-                        y0 = ceil(X2.y());
-                    } else if(X2.y() >= himg) {
-                        y0 = himg - 1;
-                    } else {
-                        y0 = 0;
-                    }
+                    x0 = ceil(X2.x());
+                    if(x0 < 0) x0 = 0;
+                    if(x0 >= wimg) x0 = wimg-1;
 
-                    if(X2.x() < wimg && X2.x() > 0.5) {
-                        x0 = ceil(X2.x());
-                    } else if(X2.y() >= wimg) {
-                        x0 = wimg - 1;
-                    } else {
-                        x0 = 0;
-                    }
+                    y0 = ceil(X2.y());
+                    if(y0 < 0) y0 = 0;
+                    if(y0 >= himg) y0 = himg-1;
 
-                    if(X2 == X)  n++;
-                    QRgb c = imgs[h]->pixel(x0, y0);
+                    X2 = QPoint(x0, y0);
+
+                    if(X2 == X) n++;
+                    QRgb c = imgs[h]->pixel(X2);
 
                     imgs[h+2]->setPixel(X, c);
 
                     if(h == 0) {
-                        QRgb d = imgs[!h]->pixel(x0, y0);
+                        QRgb d = imgs[1]->pixel(X2);
 
                         float f = 0.5;
                         float g = 1 - f;
@@ -454,26 +437,24 @@ void MainWindow::on_btnProcess_clicked() {
                         QRgb e = qRgb(f*qRed(c) + g*qRed(d),
                                       f*qGreen(c) + g*qGreen(d),
                                       f*qBlue(c) + g*qBlue(d)) ;
-                        imgs[4]->setPixel(X, qRgba(qRed(e), qGreen(e), qBlue(e), 0.6));
+                        imgs[4]->setPixel(X, qRgba(qRed(e), qGreen(e), qBlue(e), 0.2));
                     }
                 }
             }
-
-            //printf("Imagen %02d: %d / %d ", h+1, n, (wimg * himg));
         }
 
 
     } else {
-        for(int k=0; k<2; ++k) {
-            for(int i = 0, j = view[k]->listPoint->size(); i<j; ++i) {
+//        for(int k=0; k<2; ++k) {
+//            for(int i = 0, j = view[k]->listPoint->size(); i<j; ++i) {
 
-                int x = view[k]->listPoint->at(i).x();
-                int y = view[k]->listPoint->at(i).y();
+//                int x = view[k]->listPoint->at(i).x();
+//                int y = view[k]->listPoint->at(i).y();
 
-                if(x >= 0 && x < wimg && y >= 0 && y < himg)
-                    imgs[2]->setPixel(x, y, k?r:b);
-            }
-        }
+//                if(x >= 0 && x < wimg && y >= 0 && y < himg)
+//                    imgs[2]->setPixel(x, y, k?r:b);
+//            }
+//        }
     }
 
     for(int h=2; h<5; ++h) {
